0153f27ccdc6271598018fbdb341736c32806218
Toni Nađ <me@tnad.xyz>
2025-11-25 17:59:12 +0100

    Replace layout with minimal navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,121 @@
-import Frame from './Frame.jsx';
-
 export default function App() {
   return (
     <div
       style={{
-        alignItems: 'center',
-        backgroundColor: '#F7F7F7',
-        display: 'flex',
-        justifyContent: 'center',
-        minHeight: '100dvh',
-        padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
+        backgroundColor: '#FFFFFF',
+        minHeight: '100vh',
         width: '100%',
       }}
     >
-      <Frame />
+      <nav
+        style={{
+          alignItems: 'center',
+          boxSizing: 'border-box',
+          contain: 'layout',
+          display: 'flex',
+          flexDirection: 'row',
+          fontSynthesis: 'none',
+          gap: '16px',
+          height: 'fit-content',
+          justifyContent: 'start',
+          MozOsxFontSmoothing: 'grayscale',
+          paddingBlock: '20px',
+          paddingInline: '20px',
+          WebkitFontSmoothing: 'antialiased',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            alignItems: 'start',
+            boxSizing: 'border-box',
+            contain: 'layout',
+            display: 'flex',
+            flexBasis: '0px',
+            flexDirection: 'column',
+            flexGrow: '1',
+            flexShrink: '0',
+            height: 'fit-content',
+            justifyContent: 'start',
+            width: 'auto',
+          }}
+        >
+          <div
+            style={{
+              boxSizing: 'border-box',
+              color: '#000000',
+              flexShrink: '0',
+              fontFamily: '"Google Sans Flex", system-ui, sans-serif',
+              fontSize: '20px',
+              fontVariationSettings:
+                '"wght" 400, "wdth" 100, "slnt" 0, "GRAD" 0, "ROND" 0',
+              fontWeight: 400,
+              height: 'fit-content',
+              lineHeight: '140%',
+              whiteSpace: 'pre',
+              width: 'fit-content',
+            }}
+          >
+            Toni Nađ
+          </div>
+        </div>
+        <div
+          style={{
+            alignItems: 'center',
+            borderRadius: '0px',
+            boxSizing: 'border-box',
+            contain: 'layout',
+            display: 'flex',
+            flexDirection: 'row',
+            flexShrink: '0',
+            gap: '12px',
+            height: 'fit-content',
+            justifyContent: 'start',
+            paddingBlock: '0px',
+            paddingInline: '0px',
+            width: 'fit-content',
+          }}
+        >
+          <div
+            style={{
+              alignItems: 'center',
+              boxSizing: 'border-box',
+              contain: 'layout',
+              display: 'flex',
+              flexDirection: 'row',
+              flexShrink: '0',
+              height: '24px',
+              justifyContent: 'center',
+              width: '24px',
+            }}
+          >
+            <div
+              style={{
+                backgroundImage:
+                  'url(https://workers.paper.design/file-assets/01KACA23KJT6YCXQ7Y94ADCZE1/01KAXS3K8VTA6TT7AJYZJSDA0F.png)',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                boxSizing: 'border-box',
+                flexShrink: '0',
+                height: '19px',
+                width: '19px',
+              }}
+            />
+          </div>
+          <div
+            style={{
+              backgroundImage:
+                'url(https://workers.paper.design/file-assets/01KACA23KJT6YCXQ7Y94ADCZE1/01KAXS4XGXBVXT4CK0NG024Y15.svg)',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              boxSizing: 'border-box',
+              flexShrink: '0',
+              height: '24px',
+              width: '24px',
+            }}
+          />
+        </div>
+      </nav>
     </div>
   );
 }
